8712743fb9b175febf255b0931c4fabd57573296
Josh Allen <josh.allen.it@gmail.com>
2015-04-02 02:48:53 -0400

    docs

--- Game of Life - iOS Entry/LifeHttpConnection.m
+++ Game of Life - iOS Entry/LifeHttpConnection.m
@@ -16,17 +16,8 @@
 
 - (NSObject<HTTPResponse> *)httpResponseForMethod:(NSString *)method URI:(NSString *)path
 {
-    // Use HTTPConnection's filePathForURI method.
-    // This method takes the given path (which comes directly from the HTTP request),
-    // and converts it to a full path by combining it with the configured document root.
-    //
-    // It also does cool things for us like support for converting "/" to "/index.html",
-    // and security restrictions (ensuring we don't serve documents outside configured document root folder).
     
     NSString *filePath = [self filePathForURI:path];
-    
-    // Convert to relative path
-    
     NSString *documentRoot = [config documentRoot];
     
     if (![filePath hasPrefix:documentRoot])
@@ -41,7 +32,16 @@
     {
         DDLogInfo(@"%@[%p]: Serving up dynamic content", THIS_FILE, self);
         
-        NSArray *grid = @[@[@0, @0, @0],@[@0, @1, @0],@[@0, @0, @0]];
+        NSDictionary * params = [self parseGetParams];
+        NSString * gridParam = params[@"grid"];
+        NSError *jsonParsingError;
+        NSArray *grid = [NSJSONSerialization JSONObjectWithData:[gridParam dataUsingEncoding:NSUTF8StringEncoding]
+                                        options:0
+                                          error:&jsonParsingError];
+        if (jsonParsingError) {
+            NSLog(@"Could not parse grid %@ %@", gridParam, jsonParsingError);
+        }
+        
         return [[HTTPJSONResponse alloc] initWithObject:grid];
     }
     else {
---
+++ README.md
@@ -0,0 +1,6 @@
+# iOS Entry for Conway's Game of Life Simulation
+
+Edit LifeHttpConnection.m and LifeHttpConnectionTests.m to put your functionality.
+
+Otherwise this is an empty shell project.
+
